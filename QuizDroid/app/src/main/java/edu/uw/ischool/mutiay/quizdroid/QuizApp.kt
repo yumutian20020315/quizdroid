fbb95c1de5c5d914fbede6c5e999186c2cf0c497
Mutian Yu <mutiay@uw.edu>
2023-11-14 22:07:51 -0800

    storage done

--- QuizDroid/app/src/main/java/edu/uw/ischool/mutiay/quizdroid/QuizApp.kt
+++ QuizDroid/app/src/main/java/edu/uw/ischool/mutiay/quizdroid/QuizApp.kt
@@ -39,127 +39,6 @@ interface TopicRepository {
 }
 
 
-//class MockTopicRepository: TopicRepository {
-//    val items:MutableList<Topic> = mutableListOf(
-//        Topic(
-//            title = "Math",
-//            descriptionLong = "Questions about addition",
-//            descriptionShort = "Some quick questions on calculation",
-//            quizzes = mutableListOf(
-//                Quiz(
-//                    questionText = "2 plus 3 equals what?",
-//                    answers = listOf("5", "6", "7", "8"),
-//                    correctAnswerIndex = 0
-//                ),
-//                Quiz(
-//                    questionText = "5 plus 6 equals what?",
-//                    answers = listOf("11", "12", "13", "14"),
-//                    correctAnswerIndex = 0
-//                ),
-//                Quiz(
-//                    questionText = "7 plus 8 equals what?",
-//                    answers = listOf("14", "15", "16", "17"),
-//                    correctAnswerIndex = 1
-//                )
-//            ),
-//            iconId = android.R.drawable.ic_dialog_email
-//        ),
-//        Topic(
-//            title = "Physics",
-//            descriptionLong = "Question about speed and simple machine",
-//            descriptionShort = "Some quick questions on basic physics",
-//            quizzes = mutableListOf(
-//                Quiz(
-//                    questionText = "A 10 kg object is pushed with a force of 20 N. What is its acceleration?",
-//                    answers = listOf("2 meters per second squared", "5 meters per second squared", "10 meters per second squared", "20 meters per second squared"),
-//                    correctAnswerIndex = 0
-//                ),
-//                Quiz(
-//                    questionText = "A car travels 150 km in 3 hours. What is its average speed?",
-//                    answers = listOf("50 kilometers per hour", "60 kilometers per hour", "70 kilometers per hour", "80 kilometers per hour"),
-//                    correctAnswerIndex = 0
-//                ),
-//                Quiz(
-//                    questionText = "What type of simple machine is a seesaw?",
-//                    answers = listOf("Lever", "Pulley", "Wheel and Axle", "Inclined Plane"),
-//                    correctAnswerIndex = 0
-//                )
-//            ),
-//            iconId = android.R.drawable.sym_def_app_icon
-//        ),
-//        Topic(
-//            title = "Marvel Super Heroes",
-//            descriptionLong = "Question about Marval Super Heros the famous ones",
-//            descriptionShort = "Some quick questions on Marval Super Heros",
-//            quizzes = mutableListOf(
-//                Quiz(
-//                    questionText = "What is the source of the Hulk's strength?",
-//                    answers = listOf("Gamma Radiation", "Super-Soldier Serum", "Alien Technology", "Mystical Enchantment"),
-//                    correctAnswerIndex = 0
-//                ),
-//                Quiz(
-//                    questionText = "Which group of heroes does Iron Man belong to?",
-//                    answers = listOf("Justice League", "X-Men", "Avengers", "Teen Titans"),
-//                    correctAnswerIndex = 2
-//                )
-//            ),
-//            iconId = android.R.drawable.arrow_down_float
-//        ),
-//        Topic(
-//            title = "ice cream",
-//            descriptionLong = "Questions about ice cream falvors",
-//            descriptionShort = "Some quick questions on ice cream",
-//            quizzes = mutableListOf(
-//                Quiz(
-//                    questionText = "Which ice cream is white color",
-//                    answers = listOf("Chocolate", "Strawberry", "Vanilla", "Mint Chocolate Chip"),
-//                    correctAnswerIndex = 2
-//                ),
-//                Quiz(
-//                    questionText = "Which ice cream is green color",
-//                    answers = listOf("Vanilla", "Mint", "Orange", "Chocolate"),
-//                    correctAnswerIndex = 1
-//                )
-//            ),
-//            iconId = android.R.drawable.arrow_up_float
-//        )
-//    )
-//
-//    override fun saveTopic(newTopic: Topic) {
-//        items.add(newTopic)
-//    }
-//
-//    override fun saveQuiztoTopic(title: String, newQuiz: Quiz) {
-//        items.find { it.title == title }?.quizzes?.add(newQuiz)
-//    }
-//
-//    override fun getTopicsnames(): List<String> {
-//        return items.map { it.title }
-//    }
-//
-//    override fun getTitleswithDes(): List<String> {
-//        return items.map { it.title + ": " + it.descriptionShort}
-//    }
-//
-//    override fun getQuestionsnum(title: String): Int {
-//        val topic = items.find { it.title == title }
-//
-//        return topic?.quizzes?.size ?: 0
-//    }
-//
-//    override fun findTopicbyName(title: String): Topic? {
-//        return items.find { it.title == title }
-//    }
-//
-//    override fun findQuizbyIndex(title: String, index: Int): Quiz? {
-//        return findTopicbyName(title)?.quizzes?.get(index)
-//    }
-//
-//    override fun getAlltopics(): MutableList<Topic> {
-//        return items
-//    }
-//}
-
 
 
 
@@ -264,6 +143,7 @@ class MockTopicRepository(private val context: Context): TopicRepository {
             parseTopics(jsonString).let {
                 items.addAll(it)
             }
+            Log.e("MockTopicRepository", "File OK")
         } else {
             Log.e("MockTopicRepository", "File does not exist: $fileName. Using hardcoded data.")
         }
--- QuizDroid/app/src/main/java/edu/uw/ischool/mutiay/quizdroid/Setting.kt
+++ QuizDroid/app/src/main/java/edu/uw/ischool/mutiay/quizdroid/Setting.kt
@@ -1,18 +1,25 @@
 package edu.uw.ischool.mutiay.quizdroid
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.ActivityNotFoundException
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
+const val ALARM_ACTION = "edu.uw.ischool.mutiay.ALARM"
 class Setting : AppCompatActivity() {
     private lateinit var url:EditText
     private lateinit var minute_download:EditText
     private lateinit var confirm:Button
+    var receiver : BroadcastReceiver? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +32,10 @@ class Setting : AppCompatActivity() {
 
 
         confirm.setOnClickListener {
+            if(receiver !== null) {
+                unregisterReceiver(receiver)
+                receiver = null
+            }
             var url_for_download = url.text.toString()
             var minutes = minute_download.text.toString()
 
@@ -36,27 +47,29 @@ class Setting : AppCompatActivity() {
             Log.i("test", minutes)
             prefsEditor.putString("minutes_between_download", minutes)
             prefsEditor.apply()
+
+            if (receiver == null) {
+                receiver = object : BroadcastReceiver() {
+                    override fun onReceive(context: Context?, intent: Intent?) {
+                        Toast.makeText(context, "Download attempt happen: $url_for_download", Toast.LENGTH_LONG).show()
+                        Log.i("download", "start to download")
+
+                    }
+                }
+                val filter = IntentFilter(ALARM_ACTION)
+                registerReceiver(receiver, filter)
+            }
+
+            // Create the PendingIntent
+            val intent = Intent(ALARM_ACTION)
+            val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            // Get the Alarm Manager
+            val alarmManager : AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+            var miliInterval = ((minutes.toInt() ?: 0) * 60000).toLong()
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+miliInterval, miliInterval, pendingIntent)
         }
-
-//        back_main.setOnClickListener {
-//            val intent = Intent(this, MainActivity::class.java)
-//
-//            try {
-//                startActivity(intent)
-//            } catch (e: ActivityNotFoundException) {
-//
-//            }
-//        }
-
-
-
-
-
-
-
-
-
-
 
     }
 }
